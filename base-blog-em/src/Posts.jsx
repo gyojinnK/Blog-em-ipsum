7f0095cebe3f6068bfbdefc179ca7e908959260c
gyojinnK <ksmug1998@gmail.com>
2024-07-10 17:49:53 +0900

    FEAT: add react-query-devtools and practice lib

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 
 import { fetchPosts, deletePost, updatePost } from "./api";
 import { PostDetail } from "./PostDetail";
@@ -9,7 +10,29 @@ export function Posts() {
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery
-  const data = [];
+  const { data, isError, error, isLoading } = useQuery({
+    queryKey: ["posts"], // 쿼리 키는 항상 배열!
+    queryFn: fetchPosts, // 데이터를 가져오기 위한 함수
+  });
+
+  if (isLoading) {
+    // isFetching
+    // 비동기 쿼리가 아직 해결되지 않았다는 것을 말한다.
+    // 아직 fetch가 완료되지 않았지만, Axios 호출이나 GraphQL 호출 같은
+    // 다른 종류의 데이터를 가져오는 작업일 수 있다.
+    // isLoading은 isFetching의 하위 집합!
+    return <h3>Loading...</h3>;
+  }
+  if (isError) {
+    // react-query는 기본적으로 3번 시도 후
+    // error라고 판단
+    return (
+      <>
+        <h3>Oops, Error...</h3>
+        <p>{error.toString()}</p>
+      </>
+    );
+  }
 
   return (
     <>
