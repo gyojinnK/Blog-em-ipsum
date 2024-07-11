5d9ad26f4d5c83c7001da63e887986b1c88ae829
gyojinnK <ksmug1998@gmail.com>
2024-07-11 21:09:02 +0900

    FEAT: setting init

--- base-infinite-swapi/src/App.jsx
+++ base-infinite-swapi/src/App.jsx
@@ -1,13 +1,21 @@
 import "./App.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevTools } from "@tanstack/react-query-devtools";
+
 import { InfinitePeople } from "./people/InfinitePeople";
 import { InfiniteSpecies } from "./species/InfiniteSpecies";
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
     <div className="App">
-      <h1>Infinite SWAPI</h1>
-      <InfinitePeople />
-      {/* <InfiniteSpecies /> */}
+      <QueryClientProvider client={queryClient}>
+        <h1>Infinite SWAPI</h1>
+        <InfinitePeople />
+        {/* <InfiniteSpecies /> */}
+        <ReactQueryDevTools />
+      </QueryClientProvider>
     </div>
   );
 }
